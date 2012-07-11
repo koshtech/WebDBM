348d4bf7ade2735260c70de162ed90106fda3d3e
Fernando Kosh <fernando.kosh@gmail.com>
2012-07-11 20:37:04 -0300

    Added a empty Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webdbm::Application.config.secret_token = '8787d9ed684256fa5f1f88f0c70f04f75b46f1427ceebaa7e5b8424c1a6f578fd92358e1bf95517288ea7dee5d105c502f8101bb9ce34380e1c5b9a7c22ec1c1'
