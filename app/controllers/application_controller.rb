367fa908569fa801624100cf1762acc64c0a76ee
Ivan Joaquim <pichotweb@gmail.com>
2012-08-01 21:49:48 -0300

    testing authlogic in new branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user_session, :current_user
+
+  private
+    
+    def current_user_session
+    end
+	
 end
